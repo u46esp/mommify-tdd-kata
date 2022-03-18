78326a45d84fc1d39a6de75330e796e822bb9d63
Ekarin Supataratarn <ekarin.supataratarn@thoughtworks.com>
2022-03-18 07:24:51 +0700

    Use proper logic to mommify string

--- src/mommify/mommify.ts
+++ src/mommify/mommify.ts
@@ -1,7 +1,12 @@
 export const mommify = (str: string): string => {
-  switch(str) {
-    case 'a': return 'mommy'
-    case 'bla': return 'blmommy'
-    default: return str
+  const needToMommifyString = (str: string): boolean => {
+    const vowelCount = str
+      .split('')
+      .filter(char => char === 'a')
+      .length
+
+    return vowelCount > 0.3 * str.length
   }
+
+  return needToMommifyString(str) ? str.replace('a', 'mommy') : str
 };
