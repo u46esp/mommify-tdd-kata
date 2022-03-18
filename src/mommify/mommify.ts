0fe49b317f831cbd3596138aa36ac35352119751
Ekarin Supataratarn <ekarin.supataratarn@thoughtworks.com>
2022-03-18 07:46:37 +0700

    Extract and give proper names to vowel calculation step

--- src/mommify/mommify.ts
+++ src/mommify/mommify.ts
@@ -1,14 +1,14 @@
 const needToMommifyString = (str: string): boolean => {
+  const vowelThreshold = 0.3 * str.length
+  const vowels = ['a','e','i','o','u']
   const vowelCount = str
     .split('')
-    .filter(char => char === 'a' || char === 'e' || char == 'i' || char === 'o' || char === 'u')
+    .filter(char => vowels.includes(char))
     .length
 
-  return vowelCount > 0.3 * str.length
+  return vowelCount > vowelThreshold
 }
 
-
 export const mommify = (str: string): string => {
-
   return needToMommifyString(str) ? str.replace(/[(a|e|i|o|u)]+/g, 'mommy') : str
-};
+}
